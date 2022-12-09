9f581476680dbd90149d9556c324c07c4e275711
Joseph Salvatori <joe@zero.nyc>
2022-12-09 10:11:31 -0600

    events

---
+++ src/routes/api/events/inbound/+server.js
@@ -0,0 +1,22 @@
+import { json } from "@sveltejs/kit";
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+
+	let returnObj = {
+		data: {},
+		errors: []
+	};
+
+	/**
+	 * Write stuff below this
+	 */
+	console.log(request);
+
+
+
+	/**
+	 * Write stuff above this
+	 */
+	return;
+};
